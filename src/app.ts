066d993c7d13991734508b63b2f597b2bf525aff
serhiirubets <serhii.rubets@gmail.com>
2021-12-01 09:02:30 +0200

    Add exception handler

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,40 @@
 import express, {Express} from 'express';
-import { userRouter } from './users/users';
 import { Server } from 'http';
 import { LoggerService } from './logger/logger.service';
+import {UsersController} from './users/users.controller';
+import { ExeptionFilter } from './errors/exeption.filter';
 
 export class App {
   app: Express;
   port: number;
   server: Server;
   logger: LoggerService;
+  usersController: UsersController;
+  exeptionFilter: ExeptionFilter;
 
-  constructor(logger: LoggerService) {
+  constructor(
+    logger: LoggerService,
+    usersController: UsersController,
+    exeptionFilter: ExeptionFilter
+  ) {
     this.app = express();
     this.port = 8000;
     this.logger = logger;
+    this.usersController = usersController;
+    this.exeptionFilter = exeptionFilter;
   }
 
   public useRoutes() {
-    this.app.use('/users', userRouter);
+    this.app.use('/users', this.usersController.router);
+  }
+
+  useExeptionFilters() {
+    this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
   }
 
   public init() {
     this.useRoutes();
+    this.useExeptionFilters();
     this.server = this.app.listen(this.port);
     this.logger.log(`Сервер запущен на http://localhost:${this.port}`)
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { App } from './app';
 import { LoggerService } from './logger/logger.service';
+import {UsersController} from './users/users.controller';
+import { ExeptionFilter } from './errors/exeption.filter';
 
 async function bootstrap() {
-  const app = new App(new LoggerService());
+  const logger = new LoggerService();
+  const app = new App(logger, new UsersController(logger), new ExeptionFilter(logger));
   await app.init();
 }
 
